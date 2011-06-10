6c65c56d5b323e5442ef1e407bd445c06134e855
Ransford Okpoti <ranskills@yahoo.co.uk>
2011-06-10 09:42:20 +0000

    Added logging level functionality

--- com.cre4nslab.SmartLogger.js
+++ com.cre4nslab.SmartLogger.js
@@ -1,10 +1,54 @@
 var com = com || {}
 com.cre4nslab = com.cre4nslab || {}
 
-
 com.cre4nslab.SmartLogger = {
     
+    /**
+     *
+     * @class
+     * @constructor
+     * 
+     * @param {Integer} level
+     * @param {String} levelStr
+     */
+    LoggingLevel : function(level, levelStr){
+        this.level = level;
+        this.levelStr = levelStr;
+        
+        /** @return {String} */
+        this.toString = function(){ return this.levelStr; }
+
+        /** @return {Integer} */
+        this.getValue = function(){ return this.level }
+        
+        /**
+         * @return {Boolean}
+         */
+        this.isLoggable = function(){
+            var level = com.cre4nslab.SmartLogger.level;
+            
+            if(level===null) return true;
+            return level.getValue() >= this.level
+        }
+        
+        return this;
+    },
+    
+    /** @private */
+    level : null,
+    
+    /**
+     * @param {LoggingLevel} level
+     */
+    setLevel: function(level){
+        if(!(level instanceof this.Level)) return;
+        
+        this.level = level
+    },
+    
     debug: function(){
+        if(!this.DEBUG.isLoggable()) return;
+        
         try{
             console.debug.apply(this, arguments);
         }
@@ -12,6 +56,8 @@ com.cre4nslab.SmartLogger = {
     },
     
     log: function(){
+        if(!this.LOG.isLoggable()) return;
+        
         try{
             console.log.apply(this, arguments);
         }
@@ -19,6 +65,8 @@ com.cre4nslab.SmartLogger = {
     },
     
     info: function(){
+        if(!this.INFO.isLoggable()) return;
+        
         try{
             console.info.apply(this, arguments);
         }
@@ -26,6 +74,8 @@ com.cre4nslab.SmartLogger = {
     },
     
     warn: function(){
+        if(!this.WARN.isLoggable()) return;
+        
         try{
             console.warn.apply(this, arguments);
         }
@@ -33,6 +83,8 @@ com.cre4nslab.SmartLogger = {
     },
     
     error: function(){
+        if(!this.ERROR.isLoggable()) return;
+        
         try{
             console.error.apply(this, arguments);
         }
@@ -40,3 +92,10 @@ com.cre4nslab.SmartLogger = {
     }
     
 }
+
+// Defining the logging level constants
+com.cre4nslab.SmartLogger.ERROR = new com.cre4nslab.SmartLogger.LoggingLevel(1,'ERROR');
+com.cre4nslab.SmartLogger.WARN  = new com.cre4nslab.SmartLogger.LoggingLevel(2,'WARN');
+com.cre4nslab.SmartLogger.INFO  = new com.cre4nslab.SmartLogger.LoggingLevel(3,'INFO');
+com.cre4nslab.SmartLogger.DEBUG = new com.cre4nslab.SmartLogger.LoggingLevel(4,'DEBUG');
+com.cre4nslab.SmartLogger.LOG   = new com.cre4nslab.SmartLogger.LoggingLevel(5,'LOG');
